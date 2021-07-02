6ee4193bae9c41171124c4bd26996142881c4d6a
Jesse White <jesse.white6@gmail.com>
2021-07-02 09:52:25 -0700

    :beers: add edit form for tap.

---
+++ src/components/EditTapForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
+import { react } from '@babel/types';
+
+function EditTapForm(props) {
+  const { tap } = props;
+
+  function handleEditTapFormSubmission(event) {
+    event.preventDefault();
+
+    props.onEditTap({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, quantity: event.target.quantity.value, id: tap.id })
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditTapFormSubmission}
+        buttonText={"Update Tap Information"}
+      />
+    </React.Fragment>
+  )
+}
+
+EditTapForm.propTypes = {
+  tap: PropTypes.object,
+  onEditTap: PropTypes.func
+}
+
+export default EditTapForm;
