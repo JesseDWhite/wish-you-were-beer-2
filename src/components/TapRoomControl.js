61ce9beed21ae9bce70a0af059ecd7f9e8a8873e
Jesse White <jesse.white6@gmail.com>
2021-07-02 09:21:07 -0700

    :sparkles: add initial markup in control and click function.

---
+++ src/components/TapRoomControl.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import TapList from './TapList';
+import TapDetails from './TapDetails';
+
+class TapRoomControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisisbleOnPage: false,
+      masterTapList: [],
+      selectedTap: null,
+      editing: false
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedTap != null) {
+      this.setState({
+        formVisisbleOnPage: false,
+        selectedTap: null,
+        editing: false
+      })
+    } else {
+      this.setState(prevState => ({ formVisisbleOnPage: !prevState.formVisisbleOnPage }));
+    }
+  }
+}
