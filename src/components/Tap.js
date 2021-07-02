ceb21199b76eccf43f0025afb82b426d8908c882
Jesse White <jesse.white6@gmail.com>
2021-07-02 16:12:32 -0700

    :art: update to fonts.

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -5,12 +5,12 @@ function Tap(props) {
   return (
     <React.Fragment>
       <div onClick={() => props.whenTapClicked(props.id)}>
-        <div class="row">
+        <div class="card tap-card">
           <h2>{props.name}</h2>
-          <h3>{props.brand}</h3>
-          <p>{props.price}</p>
-          <p>{props.alcoholContent}</p>
-          <p>{props.quantity}</p>
+          <h3>Brewer - {props.brand}</h3>
+          <p>Price - ${props.price}</p>
+          <p>ABV - {props.alcoholContent}</p>
+          <p>Pints Left - {props.quantity}</p>
         </div>
       </div>
     </React.Fragment>
