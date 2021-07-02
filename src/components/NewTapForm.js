2e6651d8363adfa0ff666d025665d59408668d47
Jesse White <jesse.white6@gmail.com>
2021-07-02 09:43:05 -0700

    :beers: add new tap create form.

---
+++ src/components/NewTapForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReusableForm from './ReusableForm';
+import { v4 } from 'uuid';
+
+function NewTapForm(props) {
+  function handleNewTapFormSubmission(event) {
+    event.preventDefault();
+
+    props.onNewTapCreation({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, quantity: event.target.quantity.value, id: v4() });
+  };
+
+  NewTapForm.propTypes = {
+    onNewTapCreation: PropTypes.func
+  };
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewTapFormSubmission}
+        buttonText="Add Tap" />
+    </React.Fragment>
+  )
+}
+
+export default NewTapForm;
