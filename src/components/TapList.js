f7dac8404b58b9da91af6fac3867a74c34b08ebd
Jesse White <jesse.white6@gmail.com>
2021-07-02 09:08:12 -0700

    :beers: add tap list markup.

---
+++ src/components/TapList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Tap from './Tap';
+import PropTypes from 'prop-types';
+
+function TapList(props) {
+  return (
+    <React.Fragment>
+      {props.tapList.map((tap) =>
+        <Tap name={tap.name}
+          brand={tap.brand}
+          price={tap.price}
+          alcoholContent={tap.alcoholContent}
+          quantity={tap.quantity}
+          id={tap.id}
+          key={tap.id} />
+      )}
+    </React.Fragment>
+  )
+}
+
+TapList.propTypes = {
+  tapList: PropTypes.array,
+  onTapSelection: PropTypes.func
+}
+
+export default TapList;
