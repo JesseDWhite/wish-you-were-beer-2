d157d3845e641a4591b158faa55d60c54a159368
Jesse White <jesse.white6@gmail.com>
2021-07-02 15:25:35 -0700

    :art: update styles to all pages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <React.Fragment>
+      <img src="https://media.giphy.com/media/Y00c0w6xxtLn067SUi/giphy.gif" id="beer-image" />
       <h1>Wish You Were Beer</h1>
       <em>~ Life’s Too Short to Drink Cheap Beer ~</em>
     </React.Fragment>
