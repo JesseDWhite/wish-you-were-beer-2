f63b70d62ca306e11d54ea0c8dc0b521483b606c
Jesse White <jesse.white6@gmail.com>
2021-07-02 09:29:09 -0700

    :beers: add reusable form for both edit and create functionality.

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReusableForm(props) {
+  <React.Fragment>
+    <form>
+      <input
+        type='text'
+        name='name'
+        placeholder='Name of Tap'
+      />
+      <input
+        type='text'
+        name='brand'
+        placeholder='Tap Maker'
+      />
+      <input
+        type='number'
+        name='price'
+        placeholder='Price Per Pint'
+      />
+      <input
+        type='number'
+        name='alcoholContent'
+        placeholder='Alcohol Content'
+      />
+      <input
+        type='number'
+        name='quantity'
+        placeholder='Number of Pints Availible'
+      />
+    </form>
+  </React.Fragment>
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
