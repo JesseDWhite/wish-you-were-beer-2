b9ed2081f0c5904c50151abb215aac8edbf13648
Jesse White <jesse.white6@gmail.com>
2021-07-02 09:00:11 -0700

    :construction: add markup on tap component.

---
+++ src/components/Tap.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Tap(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenTapClicked(props.id)}>
+        <h2>{props.name}</h2>
+        <h3>{props.brand}</h3>
+        <p>{props.price}</p>
+        <p>{props.alcoholContent}</p>
+        <p>{props.quantity}</p>
+      </div>
+    </React.Fragment>
+  )
+}
+
+Tap.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  alcoholContent: PropTypes.number,
+  quantity: PropTypes.number,
+  whenTapClicked: PropTypes.func
+};
+
+export default Tap;
