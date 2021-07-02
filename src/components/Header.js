89036fa3d9f877166dde249e5fa95367cc30cb0b
Jesse White <jesse.white6@gmail.com>
2021-07-02 08:49:31 -0700

    move App.js into the component tree and update to inlcude header.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Wsh You Were Beer</h1>
+      <em>~ Life’s Too Short to Drink Cheap Beer ~</em>
+    </React.Fragment>
+  )
+}
+
+export default Header;
