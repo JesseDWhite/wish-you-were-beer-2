8470fad6eb1714c36f3852bf726358fea5522846
Jesse White <jesse.white6@gmail.com>
2021-07-02 11:13:49 -0700

    :pencil2: fix issue with no return statement in ReusableForm.js.

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -2,35 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ReusableForm(props) {
-  <React.Fragment>
-    <form onSubmit={props.formSubmissionHandler}>
-      <input
-        type='text'
-        name='name'
-        placeholder='Name of Tap'
-      />
-      <input
-        type='text'
-        name='brand'
-        placeholder='Tap Maker'
-      />
-      <input
-        type='number'
-        name='price'
-        placeholder='Price Per Pint'
-      />
-      <input
-        type='number'
-        name='alcoholContent'
-        placeholder='Alcohol Content'
-      />
-      <input
-        type='number'
-        name='quantity'
-        placeholder='Number of Pints Availible'
-      />
-    </form>
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name of Tap'
+        />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Tap Maker'
+        />
+        <input
+          type='number'
+          name='price'
+          placeholder='Price Per Pint'
+        />
+        <input
+          type='number'
+          name='alcoholContent'
+          placeholder='Alcohol Content'
+        />
+        <input
+          type='number'
+          name='quantity'
+          placeholder='Number of Pints Availible'
+        />
+      </form>
+    </React.Fragment>
+  )
 }
 
 ReusableForm.propTypes = {
