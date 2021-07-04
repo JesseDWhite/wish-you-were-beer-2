23083c6f29d5ccac1366858e5a903ef1fd4e35c6
Jesse White <jesse.white6@gmail.com>
2021-07-03 18:30:02 -0700

    :art: update to form field.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <React.Fragment>
-      <img src="https://media.giphy.com/media/Y00c0w6xxtLn067SUi/giphy.gif" id="beer-image" alt="an animated image of two beer mugs clicking together" />
+      <img src="https://media.giphy.com/media/Y00c0w6xxtLn067SUi/giphy.gif" id="beer-image" alt="an animation of two beer mugs clicking together" />
       <h1>Wish You Were Beer</h1>
       <em>- Life’s Too Short to Drink Cheap Beer -</em>
       <hr />
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -5,37 +5,52 @@ function ReusableForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input
-          type='text'
-          name='name'
-          placeholder='Name of Tap'
-          className='form-control mb-3'
-        />
-        <input
-          type='text'
-          name='brand'
-          placeholder='Tap Maker'
-          className='form-control mb-3'
-        />
-        <input
-          type='number'
-          name='price'
-          placeholder='Price Per Pint'
-          className='form-control mb-3'
-        />
-        <input
-          type='number'
-          name='alcoholContent'
-          placeholder='Alcohol Content'
-          className='form-control mb-3'
-        />
-        <input
-          type='number'
-          name='pints'
-          value='124'
-          placeholder='Number of Pints Availible'
-          className='form-control mb-3'
-        />
+        <div className='row mt-5'>
+          <div className="col-6">
+            <input
+              type='text'
+              name='name'
+              placeholder='Name of Tap'
+              className='form-control mb-3'
+            />
+          </div>
+          <div className="col-6">
+            <input
+              type='text'
+              name='brand'
+              placeholder='Tap Maker'
+              className='form-control mb-3'
+            />
+          </div>
+          <div className="col-4">
+            <label for='price' className='form-label'>Price</label>
+            <input
+              type='number'
+              name='price'
+              placeholder='Price Per Pint'
+              className='form-control mb-3'
+            />
+          </div>
+          <div className="col-4">
+            <label for='price' className='form-label'>Alcohol Content</label>
+            <input
+              type='number'
+              name='alcoholContent'
+              placeholder='Alcohol Content'
+              className='form-control mb-3'
+            />
+          </div>
+          <div className="col-4 mb-5">
+            <label for='price' className='form-label'>Number of Pints Availible</label>
+            <input
+              type='number'
+              name='pints'
+              value='124'
+              placeholder='Number of Pints Availible'
+              className='form-control mb-3'
+            />
+          </div>
+        </div>
         <button type="submit" className="btn btn-success btn-custom">{props.buttonText}</button>
       </form>
     </React.Fragment>
