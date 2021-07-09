71a220279d5bff7296b16ecf3e1bcf411629d0e1
Jesse White <jesse.white6@gmail.com>
2021-07-09 08:51:17 -0700

    :white_check_mark: pass failing test for adding a new tap.

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,5 +1,25 @@
-const reducer = (state = {}) => {
-  return state;
+import sampleTap from '../static/sampleTap.json'
+import * as c from '../actions/ActionTypes'
+
+const reducer = (state = sampleTap, action) => {
+  const { name, brand, price, alcoholContent, pints, id } = action;
+  switch (action.type) {
+    case c.ADD_TAP:
+      return {
+        ...state,
+        [id]: {
+          name,
+          brand,
+          price,
+          alcoholContent,
+          pints,
+          id
+        }
+      }
+    default:
+      return state;
+  }
+
 }
 
 export default reducer;
