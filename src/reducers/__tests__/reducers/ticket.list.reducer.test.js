217d3031e5055de5bcdef9b836dbdcc4316e215d
Jesse White <jesse.white6@gmail.com>
2021-07-09 08:34:29 -0700

    :white_check_mark: pass first initial test.

--- src/reducers/__tests__/reducers/ticket.list.reducer.test.js
+++ src/reducers/__tests__/reducers/ticket.list.reducer.test.js
@@ -1,1 +1,8 @@
-import tapListReducer from '../tap-list-reducer';
+import tapListReducer from '../../tap-list-reducer';
+
+describe('tapListReducer', () => {
+  let action;
+  test('Should return default state if no aciton type is recognized', () => {
+    expect(tapListReducer({}, { type: null })).toEqual({});
+  })
+})
