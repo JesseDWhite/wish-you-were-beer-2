163333e03aace5867171737442c767dfc49bc144
Jesse White <jesse.white6@gmail.com>
2021-07-09 11:36:51 -0700

    :white_check_mark: add missing tests for index-reducer.

--- src/reducers/__tests__/reducers/index-reducer.test.js
+++ src/reducers/__tests__/reducers/index-reducer.test.js
@@ -1,12 +1,23 @@
 import rootReducer from "../../index";
+import { createStore } from "redux";
+import tapListReducer from '../../tap-list-reducer';
+import formVisibleOnPage from '../../form-visible-on-page-reducer'
 
 describe("rootReducer", () => {
+  let store = createStore(rootReducer);
 
   test('Should return default state if no action type is given', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       masterTapList: {},
       formVisibleOnPage: false
     });
+  });
+  test('Check that initial state of tapListReducer matches root reducer', () => {
+    expect(store.getState().masterTapList).toEqual(tapListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleOnPage matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleOnPage(undefined, { type: null }));
   });
 
 });
