32eed6f73f127ba583cf9a3dd5d3254c21700ff2
Jesse White <jesse.white6@gmail.com>
2021-07-09 09:25:17 -0700

    add combine reducers to index.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,10 @@
-import formVisibleOnPage from './form-visible-on-page-reducer';
+import formVisibleOnPageReducer from './form-visible-on-page-reducer';
 import tapListReducer from './tap-list-reducer'
 import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleOnPageReducer,
+  masterTapList: tapListReducer
+})
+
+export default rootReducer;
