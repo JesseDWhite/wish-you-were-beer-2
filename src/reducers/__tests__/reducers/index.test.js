9078bbdf4143d12982c0f4cf561b833b2aa6842b
Jesse White <jesse.white6@gmail.com>
2021-07-09 09:52:52 -0700

    :bug: issue was using 'test' instead of 'it' in test.

--- src/reducers/__tests__/reducers/index.test.js
+++ src/reducers/__tests__/reducers/index.test.js
@@ -2,7 +2,7 @@ import * as actions from '../../../actions/index';
 import * as c from '../../../actions/ActionTypes';
 
 describe('Tap list actions', () => {
-  test('deleteTap should create DELETE_TAP action', () => {
+  it('deleteTap should create DELETE_TAP action', () => {
     expect(actions.deleteTap(1)).toEqual({
       type: c.DELETE_TAP,
       id: 1
