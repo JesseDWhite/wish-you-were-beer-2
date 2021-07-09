27a6973866e565f20548c382a1f7bb39c5f41355
Jesse White <jesse.white6@gmail.com>
2021-07-09 09:13:04 -0700

    :test_tube: add failing test for form toggle.

--- src/reducers/__tests__/reducers/form-visible-on-page.reducer.test.js
+++ src/reducers/__tests__/reducers/form-visible-on-page.reducer.test.js
@@ -5,4 +5,7 @@ describe('formVisibleOnPage', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(formVisibleOnPage(false, { type: null })).toEqual(false);
   })
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleOnPage(false, { type: c.TOGGLE_FORM })).toEqual(true)
+  })
 })
