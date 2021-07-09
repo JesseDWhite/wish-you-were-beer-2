02c31b056346c10d0c8a604f161e62ae68c77935
Jesse White <jesse.white6@gmail.com>
2021-07-09 08:46:27 -0700

    :test_tube: add failing test for adding tap.

--- src/reducers/__tests__/reducers/ticket.list.reducer.test.js
+++ src/reducers/__tests__/reducers/ticket.list.reducer.test.js
@@ -1,8 +1,42 @@
 import tapListReducer from '../../tap-list-reducer';
+import * as c from '../../../actions/ActionTypes'
 
 describe('tapListReducer', () => {
   let action;
+
+  const tapData = {
+    id: "5c495f3a-eda5-44f9-8632-10a5dce2e406",
+    name: "name",
+    brand: "brand",
+    price: 12.99,
+    alcoholContent: 6.7,
+    pints: 124
+  }
+
   test('Should return default state if no aciton type is recognized', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
+  })
+
+  test('Should successfully add anew tap to masterTapList', () => {
+    const { name, brand, price, alcoholContent, pints, id } = tapData;
+    action = {
+      type: c.ADD_TAP,
+      name,
+      brand,
+      price,
+      alcoholContent,
+      pints,
+      id,
+    };
+    expect(tapListReducer({}, action)).toEqual({
+      [id]: {
+        name,
+        brand,
+        price,
+        alcoholContent,
+        pints,
+        id
+      }
+    })
   })
 })
